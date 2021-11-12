98b3c1a77e7d7b4cc46917f9fa8772b78845045d
Tautvydas Cerniauskas <tautvydas.cer@gmail.com>
2021-11-12 17:09:10 +0000

    feat: symlink files

---
+++ setup_links.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ln -s ~/Projects/Dotfiles/lazygit/ ~/.config/
+ln -s ~/Projects/Dotfiles/nvim/ ~/.config/
+ln -s ~/Projects/Dotfiles/efm-langserver/ ~/.config/
+ln -s ~/Projects/Dotfiles/.git-templates/ ~/
+ln -s ~/Projects/Dotfiles/.bashrc ~/
